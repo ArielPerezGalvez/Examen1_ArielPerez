2104aac3a33adbcdeedb52e06dec3dd47f1464fa
ArielPerezGalvez <arielgalvez70@yahoo.es>
2018-05-25 14:04:45 -0600

    cree a clase del usuario

---
+++ Examen1/src/examen1/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ariel
+ */
+public class Usuario {
+    private String nombre;
+    private int edad;
+    private String correo;
+    private String user;
+    private String contraseña;
+    
+    
+    
+    
+}//fin de la clase
